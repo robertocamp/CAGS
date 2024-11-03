c37118037a0fbeb635bdd58bfd8c1199f9db88ea
robertc <robertc@robertcs-MacBook-Pro.local>
2024-11-03 16:56:50 -0600

    adding-custom-logger

--- main.go
+++ main.go
@@ -58,8 +58,15 @@ func main() {
 	app := fiber.New()
 
 	// add logger middleware
-	app.Use(logger.New())
+	//app.Use(logger.New())
 
+
+	app.Use(logger.New(logger.Config{
+		Format:     "[${time}] ${status} - ${method} ${path}\n",
+		TimeFormat: "2006-01-02 15:04:05",
+		TimeZone:   "Local",
+	}))
+	
 	// Register the hello route
 	routes.HelloRoute(app)
 
